486e3f9ff7bc755964da96c4005aabee182b4ad3
Kenenna-bit <kenennaegbuna@gmail.com>
2024-01-21 23:28:02 +0100

    CSC 419

--- hms-apps/identity/src/app.module.ts
+++ hms-apps/identity/src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
+  TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'hms',
+      synchronize: true,
+      autoLoadEntities: true
+  }),
 })
 export class AppModule {}
--- hms-apps/identity/src/users/users.service.ts
+++ hms-apps/identity/src/users/users.service.ts
@@ -1,93 +1,68 @@
-import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
-//import { CreateUserDto } from './dto/create-user.dto';
-//import { UpdateUserDto } from './dto/update-user.dto';
-import { CreateUserDto, PaginationDto, UpdateUserDto, Users } from '@common/hms-lib';
-import { User } from '@common/hms-lib';
-import { randomUUID } from 'crypto';
-import { Observable, Subject } from 'rxjs';
+   import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
+   import { CreateUserDto, PaginationDto, UpdateUserDto, Users } from '@common/hms-lib';
+   import { User } from './entities/user.entity';
+   import { Repository } from 'typeorm';
+   import { InjectRepository } from '@nestjs/typeorm';
+   import { randomUUID } from 'crypto';
+   import { Observable, Subject } from 'rxjs';
 
-@Injectable()
-export class UsersService implements OnModuleInit{
-  //static user data for demo purpose only
-  private readonly users:User[] = [];
+   @Injectable()
+   export class UsersService implements OnModuleInit {
 
-  onModuleInit() {
-      for (let i=0; i <= 100; i++){
-        let createUserDto: CreateUserDto = {
-          primaryEmailAddress: `piosystems${i}@yahoo.co.uk`,
-          passwordHash: randomUUID(),
-          firstName: `Pio${i}`,
-          lastName: `Systems${i}`
-        }
-        this.create(createUserDto)
-      }
-  }
-  create(createUserDto: CreateUserDto): User {
-    const user:User = { //these should be from entity
-      ...createUserDto,
-      id: randomUUID(),
-      primaryEmailAddress: createUserDto.primaryEmailAddress,
-      firstName: createUserDto.firstName,
-      lastName: createUserDto.lastName,
-      backupEmailAddress: '',
-      phone: {},
-      isPrimaryEmailAddressVerified: false,
-      isBackupEmailAddressVerified: false,
-      passwordHash: randomUUID()
+     constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
+
+     onModuleInit() {
+
+     }
+
+     async create(createUserDto: CreateUserDto): Promise<User> {
+       const user = this.userRepository.create({
+    ...createUserDto,
+         id: randomUUID(),
+         backupEmailAddress: '',
+         phone: {},
+         isPrimaryEmailAddressVerified: false,
+         isBackupEmailAddressVerified: false,
+         passwordHash: randomUUID(),
+       });
+       return this.userRepository.save(user);
+     }
+
+     async findAll(): Promise<Users> {
+       const users = await this.userRepository.find();
+       return { users };
+     }
+
+     async findOne(id: string): Promise<User | undefined> {
+       return this.userRepository.findOne(id);
+     }
+
+     async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+       const user = await this.userRepository.findOne(id);
+       if (user) {
+         this.userRepository.merge(user, updateUserDto);
+         return this.userRepository.save(user);
+       }
+       throw new NotFoundException(`User not found by id ${id}`);
+     }
+
+     async remove(id: string): Promise<User> {
+       const user = await this.userRepository.findOne(id);
+       if (user) {
+         await this.userRepository.remove(user);
+         return user;
+       }
+       throw new NotFoundException(`User not found by id ${id}`); }
+
+       queryUsers(paginationDtoStream: Observable<PaginationDto>): Observable<Users> {
+         // Your existing logic
+         return someObservable;
+       }
+  
+       async findOneUserByPrimaryEmailAddress(primaryEmailAddress: string): Promise<User | undefined> {
+         return this.userRepository.findOne({ primaryEmailAddress });
+       }
     }
-    this.users.push(user);
-    return user;
-  }
+  
 
-  findAll(): Users {
-    return {users: this.users};
-  }
-
-  findOne(id: string): User {
-    return this.users.find((user) => user.id === id);
-  }
-
-  update(id: string, updateUserDto: UpdateUserDto): User {
-    const userIndex = this.users.findIndex((user) => user.id === id);
-    if (userIndex !== -1){
-      this.users[userIndex] = {
-        ...this.users[userIndex],
-        ...updateUserDto
-      }
-      return this.users[userIndex]
-    }
-    throw new NotFoundException(`User not found by id ${id}`);
-  }
-
-  remove(id: string) {
-    const userIndex = this.users.findIndex((user) => user.id === id);
-    if (userIndex !== -1){
-      return this.users.splice(userIndex)[0];
-    }
-    throw new NotFoundException(`User not found by id ${id}`);
-  }
-
-  queryUsers(paginationDtoStream: Observable<PaginationDto>): Observable<Users>{
-    const subject = new Subject<Users>();
-    const onNext = (paginationDto: PaginationDto) => {
-      const start = paginationDto.page * paginationDto.skip;
-      subject.next({
-        users: this.users.slice(start, start + paginationDto.skip)
-      });
-    };
-
-    const onComplete = () => subject.complete();
-
-    paginationDtoStream.subscribe({
-      next: onNext,
-      complete: onComplete
-    });
-
-    return subject.asObservable();
-
-  }
-
-  findOneUserByPrimaryEmailAddress(primaryEmailAddress: string): User {
-    return this.users.find((user) => user.primaryEmailAddress === primaryEmailAddress);
-  }
-}
+ 
